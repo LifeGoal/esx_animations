9338af6ea58507415d3116fff3934b76de96c128
Viktor <35453587+LifeGoal@users.noreply.github.com>
2018-12-27 20:56:05 +0100

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -3,7 +3,7 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 AddEventHandler("playerConnecting", function(name)
     local identifier = GetPlayerIdentifiers(source)[1]
-    local result = MySQL.Sync.fetchScalar("SELECT * FROM favoriteanimation WHERE identifier = @identifier", {['@identifier'] = identifier})
+    local result = MySQL.Async.fetchAll("SELECT * FROM `favoriteanimation` WHERE `identifier` = @identifier", {['@identifier'] = identifier})
     if not result then
 	MySQL.Sync.execute("INSERT INTO favoriteanimation (`identifier`) VALUES (@identifier)",{['@identifier'] = identifier})
     end
